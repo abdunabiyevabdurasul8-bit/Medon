3ec0c631919866c70a46d1d77dda747bdda0125b
abdunabiyevabdurasul8-bit <abdunabiyevabdurasul8@gmail.com>
2026-09-06 18:29:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4243,7 +4243,7 @@ async def admin_callback(update, context):
     elif d == "a_sync":
 
         await q.message.reply_text(
-            "katalogi yangilanmoqda..."
+            "kuting"
         )
 
         ok, result = sync_catalog()
