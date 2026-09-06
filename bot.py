61809ec265e37116bac7152ad9ffa8f40ee6d35f
abdunabiyevabdurasul8-bit <abdunabiyevabdurasul8@gmail.com>
2026-09-06 18:25:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ from telegram.ext import (
 # SOZLAMALAR
 # ============================================================
 
-BOT_TOKEN = "8611684086:AAFhtT3TBGlYZCJ8_l0JydrIyEuZNpo2s_k"
+BOT_TOKEN = "8799964859:AAG1J91dZe-veFaRFPBkDsFmEd2t8Z8pUF0"
 
 # Telegram ID raqamingiz
 ADMIN_ID = 5692925792
