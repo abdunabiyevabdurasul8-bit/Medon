20990af223fac136cf944d3f4f1f747c8f36b167
abdunabiyevabdurasul8-bit <abdunabiyevabdurasul8@gmail.com>
2026-09-07 12:00:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ from telegram.ext import (
 # SOZLAMALAR
 # ============================================================
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "BOT_TOKEN").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 
 ADMIN_ID_RAW = os.getenv("ADMIN_ID", "5692925792").strip()
 
